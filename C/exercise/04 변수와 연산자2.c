55d1b29d1883ad90f5146987957d86da74fa3c2e
hayoung <wizardlover_@naver.com>
2018-09-08 23:48:32 +0900

    feat: Add exercise no.4 (C)

---
+++ C/exercise/04 변수와 연산자2.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	int num1,num2;
+
+	printf("첫 번째 정수를 입력하세요: ");
+	scanf("%d", &num1);
+	printf("두 번째 정수를 입력하세요: ");
+	scanf("%d", &num2);
+
+	printf("두 수의 뺄셈의 결과는 %d입니다. \n", num1-num2);
+	printf("두 수의 곱셈의 결과는 %d입니다. \n", num1*num2);
+	return 0;
+}
