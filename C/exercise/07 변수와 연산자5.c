bc6e7b07608bcd61949b0c5b6e90bbdab6f699da
hayoung <wizardlover_@naver.com>
2018-09-12 23:23:49 +0900

    feat: Add exercise no.7 (C)

---
+++ C/exercise/07 변수와 연산자5.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	int num1,num2;
+	printf("첫 번째 정수를 입력하세요:");
+	scanf("%d", &num1);
+	printf("두 번째 정수를 입력하세요:");
+	scanf("%d", &num2);
+
+	printf("두 정수를 나누었을 때, 몫은 %d, 나머지는 %d입니다.\n", num1/num2, num1%num2);
+	return 0;
+}
