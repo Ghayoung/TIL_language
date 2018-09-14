9c67b65e7b7d275de412304172c53b67696203d3
hayoung <wizardlover_@naver.com>
2018-09-14 21:17:54 +0900

    feat: Add exercise no.8 (C)

---
+++ C/exercise/08 변수와 연산자6.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	int num1,num2,num3;
+	int result;
+
+	printf("첫 번째 정수를 입력하세요:");
+	scanf("%d", &num1);
+	printf("두 번째 정수를 입력하세요:");
+	scanf("%d", &num2);
+	printf("세 번째 정수를 입력하세요:");
+	scanf("%d", &num3);
+
+	result=(num1-num2)*(num2+num3)*(num3%num1);
+
+	printf("연산 결과: %d \n", result);
+	return 0;
+}
