1763e4e6cc29cd6699fdad891528888b1b97e1a3
hayoung <wizardlover_@naver.com>
2018-09-21 21:07:45 +0900

    Add exercise no.13 (C)

---
+++ C/exercise/13 포인터와 배열.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	int arr[7] = {1,2,3,4,5,6,7}; //길이가 7인 int형 배열 선언 후 초기화
+	int *ptr = arr; //배열의 첫 번째 요소를 가리키는 포인터 변수 ptr 선언
+	int i;
+
+	for(i=0; i<7; i++)
+	{
+		*ptr += 2; //모든 배열요소의 값 2씩 증가
+		ptr++;
+	}
+
+	for(i=0; i<7; i++)
+		printf("%d ", arr[i]); //증가가 이뤄졌는지 확인
+
+	printf("\n");
+	return 0;
+}
