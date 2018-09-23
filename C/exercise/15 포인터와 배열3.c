302af55967b38f3decaa0bbbfe962b7fa03418b2
hayoung <wizardlover_@naver.com>
2018-09-23 23:48:55 +0900

    Add exercise no.15 (C)

---
+++ C/exercise/15 포인터와 배열3.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+
+int main(void)
+{
+	int arr[7] = {1,2,3,4,5,6,7}; //길이가 7인 int형 배열 선언 후 초기화
+	int *ptr = &arr[6]; //배열의 마지막 요소를 가리키는 포인터 변수 ptr 선언
+	int total = 0; //합을 저장할 변수
+	int i;
+
+	for(i=0; i<7; i++)
+		total += *(ptr--);
+
+	printf("합 : %d \n", total);
+	return 0;
+}
