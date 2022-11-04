607014a8bc50dcf113990b36f275ea3adacafd6c
Nam Hayoung <33112915+Ghayoung@users.noreply.github.com>
2022-11-04 13:57:54 +0900

    update pythonicCode.py

--- Python/python_for_ML/pythonicCode.py
+++ Python/python_for_ML/pythonicCode.py
@@ -1,4 +1,11 @@
 colors = ["red", "blue", "green", "yellow"]
-result = " ".join(colors)
 
+#일반 코드
+result = ""
+for s in colors:
+    result += s
+print(result)
+
+#pythonic code
+result = "".join(colors)
 print(result)
